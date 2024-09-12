31f545477fbfc57835198ab85e0bc93baf6b8d9d
toia-19 <vickingiediaz19@gmail.com>
2024-09-12 20:08:19 -0300

    formDatos aplicado a formulario

--- pages/formulario.js
+++ pages/formulario.js
@@ -1,4 +1,26 @@
-/* Botón para formulario */
-document.getElementById('btn-enviar').addEventListener('click', () => {
-    fetch('http://127.0.0.1:5500/index.html')}
-);
+const formDatos = document.querySelector("#formulario");
+
+const imprimirDatos = (event) => {
+    event.preventDefault(); // evita que se envíe de forma predeterminada
+
+    const info = new FormData(event.target); // crea nuevo objeto tipo FormData con los valores recibidos del evento
+
+    const datos = Object.fromEntries(info.entries()); // transforma los valores de cada input en un objeto tipo clave-valor
+
+    console.log(JSON.stringify(datos)); // muestra por consola los datos del objeto transformados en JSON
+}
+
+formDatos.addEventListener("submit", imprimirDatos); // escucha el evento "submit" (del botón) y ejecuta la función imprimirDaots
+
+/**
+ * Resultado esperado 1: objeto
+ * {
+ *     nombre: "Maia",
+ *     apellido: "Ulloa"
+ * }
+ * 
+ * Resultado esperado 2: JSON
+ * {
+ *     "nombre": "Maia", "apellido": "Ulloa"
+ * }
+ */
